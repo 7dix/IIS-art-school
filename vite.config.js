7899d85c9fcc58697ef619cc9d5ede0ffad3e159
natkasobi <xsobih00@vutbr.cz>
2024-11-21 16:30:00 +0100

    fix: update Vite server configuration to use localhost instead of 0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0',
+        host: 'localhost',
         port: 5173,
         hmr: {
-          host: '0.0.0.0',
+          host: 'localhost',
         },
       },
     plugins: [

665bf0deeb15a8598c5ae0b0c1d9cb559275b238
7dix <tenoramarek@gmail.com>
2024-11-23 21:24:08 +0100

    feat: erd added

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        $user->attachRole('user');
+        $user->assignRole('user');
 
         event(new Registered($user));
 
